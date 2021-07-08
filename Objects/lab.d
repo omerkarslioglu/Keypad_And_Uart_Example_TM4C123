0d3f4c9b3e0e2bbb122b0e6b52e53d2eb2776c81
omerkarslioglu <omerkarsliogluu@gmail.com>
2021-07-08 19:15:57 +0300

    Lokalden eklendi .

---
+++ Objects/lab.d
@@ -0,0 +1,13 @@
+.\objects\lab.o: lab.c
+.\objects\lab.o: C:\Users\solid\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h
+.\objects\lab.o: C:\Users\solid\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm4.h
+.\objects\lab.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\lab.o: C:\Users\solid\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\lab.o: C:\Users\solid\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\lab.o: C:\Users\solid\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\lab.o: C:\Users\solid\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\lab.o: C:\Users\solid\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h
+.\objects\lab.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\lab.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\lab.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\lab.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
